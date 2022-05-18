18dfbaa29b466371f97e088a23cf6d214feff7c3
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-18 11:31:50 +0300

    Add navigation links to admin dashboard

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,15 @@ def index():
 @app.route('/admin')
 def admin_home():
     return render_template('admin_index.html')
+
+@app.route('/donor')
+def donor():
+    return render_template('donors.html')
+
+@app.route('/receiver')
+def receiver():
+    return render_template('receivers.html')
+
+@app.route('/request')
+def request():
+    return render_template('request.html')
