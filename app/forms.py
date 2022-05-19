0a27323cb12dd707a8e9e7bda59603d3e404f4e7
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-05-19 14:50:29 +0300

    Add style adminlogin page

--- app/forms.py
+++ app/forms.py
@@ -4,10 +4,8 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username',validators=[DataRequired(),Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     remember = BooleanField('Remember Me')
     submit = SubmitField('login') 
     
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from app import app
-from forms import LoginForm
+from app.forms import LoginForm
+
 
 @app.route('/')
 def index():
@@ -11,10 +12,22 @@ def admin_home():
     return render_template('admin_index.html')
 
 
-@app.route('/login')
-def login():
+@app.route('/adminlogin', methods=['GET','POST'])
+def adminlogin():
     form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
+    
+    if form.validate_on_submit():
+        print(form.email.data)
+        print(form.password.data)
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('admin_home'))
+        else:
+            flash('Login Unsuccesfull. Please check username and password', 'danger')
+        
+    return render_template('adminlogin.html', title='admin Login', form=form)
+
+
 @app.route('/donor')
 def donor():
     return render_template('donors.html')
